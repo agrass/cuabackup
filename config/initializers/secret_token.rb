9718958fae0da99b1ba80ee4c53904b6e4ccb3d8
agrass <agrass@agrass-HP-Pavilion-dm4-Notebook-PC.(none)>
2013-04-10 11:15:43 -0400

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CUA::Application.config.secret_token = '7e057c34320cc59b8f633e787819dbf6147f391619d573b9399009b10362467f2b6cbf3da3ecbc0388529ba3ad52be234fe418792e060c11c6d59c119b45ff1e'
