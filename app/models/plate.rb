050dd0132c55f9b46ad0c111a32d5f3b2fb69f80
Francisco Caiceo <jfcaiceo55@gmail.com>
2013-04-22 16:40:21 -0400

    gema nested form

--- app/models/plate.rb
+++ app/models/plate.rb
@@ -1,9 +1,9 @@
 class Plate < ActiveRecord::Base
-  attr_accessible :calorias, :nombre, :tipo, :regime_ids
+  attr_accessible :calorias, :nombre, :tipo, :regime_ids, :horario, :plate_ingredients_attributes
 
   has_many :plate_ingredients
   has_many :ingredients, :through => :plate_ingredients
-
+  accepts_nested_attributes_for :plate_ingredients, :allow_destroy => true
   
   has_many :regime_plates
   has_many :regimes, :through => :regime_plates
