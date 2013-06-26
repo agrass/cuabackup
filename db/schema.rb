1080c5e9c1d865980505423276edee26ae8d732a
Luis Hernan Paul <lhpaul11@gmail.com>
2013-06-26 15:25:05 -0400

    detalles de los pacientes listo

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130611004322) do
+ActiveRecord::Schema.define(:version => 20130626173509) do
 
   create_table "areas", :force => true do |t|
     t.string   "nombre"
     t.text     "descripcion"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "areas_plates", :id => false, :force => true do |t|
+    t.integer "area_id"
+    t.integer "plate_id"
   end
 
   create_table "day_regimes", :force => true do |t|
@@ -33,7 +38,6 @@ ActiveRecord::Schema.define(:version => 20130611004322) do
     t.string   "unidad"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.integer  "area_id"
   end
 
   create_table "order_lists", :force => true do |t|
@@ -65,6 +69,7 @@ ActiveRecord::Schema.define(:version => 20130611004322) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "rut"
+    t.text     "detalles"
   end
 
   create_table "plate_ingredients", :force => true do |t|
