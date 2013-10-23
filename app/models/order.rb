d61638a57f614665d3d2ddc8a2c7472e0012351e
Francisco Caiceo <jfcaiceo55@gmail.com>
2013-10-23 14:38:33 -0300

    estados ordenes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,32 @@
 class Order < ActiveRecord::Base
-  attr_accessible :comentarios, :horario, :order_list_id, :regime_id
+  attr_accessible :comentarios, :horario, :order_list_id, :regime_id, :estado
+  before_save :set_state
+
+  module Estados
+    PorImprimir = 1
+    Modificado = 2
+    Ok = 3
+    IngresadoAlerta = 4
+    Pendiente = 5
+  end
+
+  private
+  def set_state
+    if self.estado == Estados::Ok
+    	self.estado = Estados::Modificado
+    else
+    	date = OrderList.find(self.order_list_id).fecha
+    	if date == Date.today
+    		self.estado = Estados::IngresadoAlerta
+    	else
+    		self.estado = Estados::PorImprimir
+    	end
+    end
+  end
+
+  def set_ok
+  	self.estado = Estados::Ok
+  end
 
   has_and_belongs_to_many :plates
   belongs_to :order_lists
