57f9cead0be61f7bf38ee48fdabc8ac780176062
Jose Francisco Caiceo <jfcaiceo55@gmail.com>
2014-03-05 18:39:06 -0300

    cambios perfil nutricionista

--- app/models/change_log.rb
+++ app/models/change_log.rb
@@ -1,7 +1,7 @@
 class ChangeLog < ActiveRecord::Base
   attr_accessible :horario, :plate_id, :tipo, :user_id
   ## DESCRIPCION CHANGE LOG:
-  # Este modulo sirve para determinar cuando ubo un cambio en las ordenes y saber especificamente que plato fue el que se agrego o borro
+  # Este modulo sirve para determinar cuando hubo un cambio en las ordenes y saber especificamente que plato fue el que se agrego o borro
   # esta cuenta de lo siguente: 
   # Horario: tipo de orden que fue modificada, desayuno, almuerzo, cena
   # plate_id: se hace referencia al plato que fue modificado
