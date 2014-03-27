8cd068c25a2cdc26364ee7d6fa62cd467e04287a
Antonio Grass <antonio@antonio-HP-Pavilion-dm4-Notebook-PC.(none)>
2014-03-27 20:31:20 -0300

    cambios alertas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,7 +29,7 @@ $(function() {
     $('.edit_regime').submit();
   });
 
-  $('.icon-warning-sign').click(function(){
+  $('.notification-bubble').click(function(){
 	var pos = $('.icon-warning-sign').offset();
 	$('.notification-list-wrapper').css({top: pos.top + 30, left: pos.left - 135});
 	$('.notification-list-wrapper').show();	
