677cd8c6c9b10d360c6af90c876ce343c6ac5170
Antonio Grass <antonio_grass@hotmail.com>
2014-05-15 20:47:46 -0400

    solve bug multiples regimes

--- app/models/area.rb
+++ app/models/area.rb
@@ -3,6 +3,6 @@ class Area < ActiveRecord::Base
   has_and_belongs_to_many :plates
   
   def self.get_plates_by_horario_and_area(horario, area, fecha)
-  	OrderList.joins(orders: [plates: [:areas, :regimes]]).where(:fecha => fecha, orders: {:horario => horario.to_i}, areas: {:id => area.to_i}).select('plates.nombre as plato, count(*) as numero, regimes.nombre as regimen').group("regimes.nombre, plates.id")
+  	OrderList.joins(orders: [plates: [:areas, :regimes]]).where(:fecha => fecha, orders: {:horario => horario.to_i}, areas: {:id => area.to_i}, regime_plates: { :horario => horario.to_i, :dia =>  DayRegime.getDayByDate(Date.parse(fecha)) }).select('plates.nombre as plato, count(*) as numero, regimes.nombre as regimen').group("regimes.nombre, plates.id")
   end
 end
