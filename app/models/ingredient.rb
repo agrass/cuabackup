56163b0b16d118a34cae47882e809e6adee7bec0
Antonio Grass <antonio_grass@hotmail.com>
2014-07-01 20:49:36 -0400

    validate size

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,5 @@ class Ingredient < ActiveRecord::Base
   has_many :plate_ingredients
   has_many :plates, :through => :plate_ingredients
   has_one :area
+  validates_length_of :unidad, :minimum => 2, :maximum => 10, :allow_blank => false
 end
